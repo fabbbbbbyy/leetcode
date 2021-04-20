bd29df67fe9d699105475c2f5f1e2f5061f6acf2
fabbbbbbyy <fabianchia97@gmail.com>
2021-04-20 16:50:45 +0800

    Add TwoSum solution

---
+++ Solutions/src/TwoSum.java
@@ -0,0 +1,33 @@
+/**
+ * Problem:
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ */
+
+/* For submission, replace class TwoSum with Solution in Leetcode.*/
+
+import java.util.*;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        int[] answer = new int[2];
+        answer[0] = -1;
+        answer[1] = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i) {
+                answer[0] = i;
+                answer[1] = map.get(complement);
+                break;
+            }
+        }
+        return answer;
+    }
+}
