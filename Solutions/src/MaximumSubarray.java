fe00e36c5fb32d84f49223b9469f9a85e3e2761d
fabbbbbbyy <fabianchia97@gmail.com>
2021-04-20 22:18:43 +0800

    Add MaximumSubarray solution

--- Solutions/src/MaximumSubarray.java
+++ Solutions/src/MaximumSubarray.java
@@ -6,7 +6,7 @@
  * Insight: Contiguous subarrays tend to involve either the Kadene's or Sliding Window technique.
  * For this problem, we restart the contiguous section of elements if adding the current element
  * gives us a smaller value than before. We do not have to worry about the global maxSum because
- * it is untouched. 
+ * it is untouched.
  */
 
 /* For submission, replace classname with Solution in Leetcode.*/
