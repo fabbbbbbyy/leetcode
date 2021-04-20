6a5b28e6404df9a25da82c479d57e39b3f8e9522
fabbbbbbyy <fabianchia97@gmail.com>
2021-04-20 19:14:43 +0800

    Add MaximumSubarray solution

--- Solutions/src/MaximumSubarray.java
+++ Solutions/src/MaximumSubarray.java
@@ -4,6 +4,9 @@
  * the largest sum and return its sum.
  *
  * Insight: Contiguous subarrays tend to involve either the Kadene's or Sliding Window technique.
+ * For this problem, we restart the contiguous section of elements if adding the current element
+ * gives us a smaller value than before. We do not have to worry about the global maxSum because
+ * it is untouched. 
  */
 
 /* For submission, replace classname with Solution in Leetcode.*/
