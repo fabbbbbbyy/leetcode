7653f5495cd5e51f10c06dfacac5b8065b51fc5b
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-05-17 13:27:42 +0800

    Add LinkedListCycle solution

---
+++ Solutions/src/LinkedListCycle/LinkedListCycle.java
@@ -0,0 +1,30 @@
+package LinkedListCycle;
+
+/* For submission, replace classname with Solution in Leetcode.*/
+
+import java.util.*;
+
+public class LinkedListCycle {
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+          val = x;
+          next = null;
+        }
+    }
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
