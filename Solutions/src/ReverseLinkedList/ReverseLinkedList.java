249b78678c1e06f19906f42f10a7fb7d0b0334fb
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-05-17 13:12:40 +0800

    Add ReverseLinkedList solution

---
+++ Solutions/src/ReverseLinkedList/ReverseLinkedList.java
@@ -0,0 +1,29 @@
+package ReverseLinkedList;
+
+/* For submission, replace classname with Solution in Leetcode.*/
+
+import java.util.*;
+
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode nextNode = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+
+        return prev;
+    }
+}
