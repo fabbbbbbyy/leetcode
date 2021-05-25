736c588266069d208eeb4315ab23c45993fc3b3c
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-05-25 20:26:01 +0800

    Add ToLowerCase solution

---
+++ Solutions/src/ToLowerCase/ToLowerCase.java
@@ -0,0 +1,11 @@
+package ToLowerCase;
+
+/* For submission, replace classname with Solution in Leetcode.*/
+
+import java.util.*;
+
+public class ToLowerCase {
+    public String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+}
