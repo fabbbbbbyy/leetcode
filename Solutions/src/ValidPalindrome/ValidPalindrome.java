71019c9453478cd964dd79d35c98f0ae8b03b9dc
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-05-28 11:58:59 +0800

    Add ValidPalindrome solution

---
+++ Solutions/src/ValidPalindrome/ValidPalindrome.java
@@ -0,0 +1,21 @@
+package ValidPalindrome;
+
+/* For submission, replace classname with Solution in Leetcode.*/
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char character = s.charAt(i);
+            if (Character.isLetterOrDigit(character)) {
+                sb.append(Character.toLowerCase(character));
+            }
+        }
+
+        String original = sb.toString();
+        String reversed = sb.reverse().toString();
+
+        return original.equals(reversed);
+    }
+}
