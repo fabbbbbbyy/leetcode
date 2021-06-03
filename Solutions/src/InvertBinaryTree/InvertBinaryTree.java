c41dbafaa715deda12137fa3b7f3794e09db9748
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-06-03 15:10:39 +0800

    Add InvertBinaryTree solution

---
+++ Solutions/src/InvertBinaryTree/InvertBinaryTree.java
@@ -0,0 +1,29 @@
+package InvertBinaryTree;
+
+/* For submission, replace classname with Solution in Leetcode.*/
+
+public class InvertBinaryTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+        }
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null || root.left == null && root.right == null) {
+            return root;
+        }
+
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+
+        return root;
+    }
+}
