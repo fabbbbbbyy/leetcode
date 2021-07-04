3799a4bd05fadfe09691b36514efeb6c2c0f1e23
DESKTOP-GKSFTIM\User <fabianchia97@gmail.com>
2021-07-04 15:09:42 +0800

    Add SortList solution

---
+++ Solutions/src/SortList/SortList.java
@@ -0,0 +1,63 @@
+package SortList;
+
+/* For submission, replace classname with Solution in Leetcode.*/
+
+import java.util.*;
+
+public class SortList {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = getMid(head);
+        ListNode left = sortList(head);
+        ListNode right = sortList(mid);
+        return merge(left, right);
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        if (list1 != null) {
+            curr.next = list1;
+        } else {
+            curr.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    private ListNode getMid(ListNode node) {
+        ListNode slow = node;
+        ListNode fast = node;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode temp = slow.next;
+        slow.next = null;
+        return temp;
+    }
+}
